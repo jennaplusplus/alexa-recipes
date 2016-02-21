c84a1a4efdfad8b1613032941ec1f17eb268da2f
Jenna Nichols <jennaplusplus@gmail.com>
2016-02-20 17:07:32 -0800

    response for any request

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,4 +1,22 @@
 class WelcomeController < ApplicationController
   def index
   end
+
+  def ask
+    response = {
+      "version": "0.1.1",
+      "response": {
+        "outputSpeech": {
+          "type": "PlainText",
+          "text": "Welcome to recipes!"
+        },
+        "card": null,
+        "reprompt": null,
+        "shouldEndSession": true
+      },
+      "sessionAttributes": {}
+    }
+
+    render :json => response.as_json, status: :ok
+  end
 end
