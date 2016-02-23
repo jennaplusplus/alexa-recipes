0bd8aa3691134fbb61f10d943b3970df4b3f687b
Jenna Nichols <jennaplusplus@gmail.com>
2016-02-23 10:33:34 -0800

    restructure methods

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -19,38 +19,77 @@ class Recipe
     return response
   end
 
-  def self.begin(params)
+  def self.router(params)
     if params["request"]["type"] == "LaunchRequest"
-      response = {
-        "version": "0.1.1",
-        "response": {
-          "outputSpeech": {
-            "type": "PlainText",
-            "text": "Welcome to Recipes! Would you like a list of ingredients?"
-          },
-          "card": nil,
-          "reprompt": nil,
-          "shouldEndSession": false
-        },
-        "sessionAttributes": {"question": "list of ingredients"}
-      }
-      return response
+      self.launch
     elsif params["request"]["type"] == "IntentRequest"
-      if params["request"]["intent"]["name"] == "AMAZON.YesIntent"
-        if params["session"]["attributes"]["question"] == "list of ingredients"
-          Recipe.ingredient_list
-        end
-      elsif params["request"]["intent"]["name"] == "AMAZON.NoIntent"
-        return Recipe.build_response("No.")
-      elsif params["request"]["intent"]["name"] == "IngredientList"
+      self.intents(params)
+    end
+  end
+
+  def self.launch
+    response = {
+      "version": "0.1.1",
+      "response": {
+        "outputSpeech": {
+          "type": "PlainText",
+          "text": "Welcome to Recipes! Would you like a list of ingredients?"
+        },
+        "card": nil,
+        "reprompt": nil,
+        "shouldEndSession": false
+      },
+      "sessionAttributes": {"question": "list of ingredients"}
+    }
+    return response
+  end
+
+  def self.intents(params)
+    if params["request"]["intent"]["name"] == "AMAZON.YesIntent"
+      if params["session"]["attributes"]["question"] == "list of ingredients"
         Recipe.ingredient_list
       end
+    elsif params["request"]["intent"]["name"] == "AMAZON.NoIntent"
+      return Recipe.build_response("Ok.")
+    elsif params["request"]["intent"]["name"] == "IngredientList"
+      Recipe.ingredient_list
     end
   end
 
   def self.ingredient_list
     return Recipe.build_response("Here are your ingredients.")
   end
+
+
+  # def self.begin(params)
+  #   if params["request"]["type"] == "LaunchRequest"
+  #     response = {
+  #       "version": "0.1.1",
+  #       "response": {
+  #         "outputSpeech": {
+  #           "type": "PlainText",
+  #           "text": "Welcome to Recipes! Would you like a list of ingredients?"
+  #         },
+  #         "card": nil,
+  #         "reprompt": nil,
+  #         "shouldEndSession": false
+  #       },
+  #       "sessionAttributes": {"question": "list of ingredients"}
+  #     }
+  #     return response
+  #   elsif params["request"]["type"] == "IntentRequest"
+  #     if params["request"]["intent"]["name"] == "AMAZON.YesIntent"
+  #       if params["session"]["attributes"]["question"] == "list of ingredients"
+  #         Recipe.ingredient_list
+  #       end
+  #     elsif params["request"]["intent"]["name"] == "AMAZON.NoIntent"
+  #       return Recipe.build_response("Ok.")
+  #     elsif params["request"]["intent"]["name"] == "IngredientList"
+  #       Recipe.ingredient_list
+  #     end
+  #   end
+  # end
+
 
 
 end
