d3ef7c576579da31b224f255d4da42e7a5a7a563
Jenna Nichols <jennaplusplus@gmail.com>
2016-02-23 09:40:59 -0800

    trying yes/no intents

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -2,6 +2,23 @@ class Recipe
   include Mongoid::Document
   field :name, type: String
 
+  def self.build_response(text)
+    response = {
+      "version": "0.1.1",
+      "response": {
+        "outputSpeech": {
+          "type": "PlainText",
+          "text": text
+        },
+        "card": nil,
+        "reprompt": nil,
+        "shouldEndSession": true
+      },
+      "sessionAttributes": {}
+    }
+    return response
+  end
+
   def self.begin(params)
     if params["request"]["type"] == "LaunchRequest"
       response = {
@@ -9,30 +26,24 @@ class Recipe
         "response": {
           "outputSpeech": {
             "type": "PlainText",
-            "text": "Welcome to Recipes!"
+            "text": "Welcome to Recipes! Would you like a list of ingredients?"
           },
           "card": nil,
           "reprompt": nil,
-          "shouldEndSession": true
+          "shouldEndSession": false
         },
-        "sessionAttributes": {}
+        "sessionAttributes": {"question": "list of ingredients"}
       }
+      return response
     elsif params["request"]["type"] == "IntentRequest"
-      response = {
-        "version": "0.1.1",
-        "response": {
-          "outputSpeech": {
-            "type": "PlainText",
-            "text": "Here are your ingredients."
-          },
-          "card": nil,
-          "reprompt": nil,
-          "shouldEndSession": true
-        },
-        "sessionAttributes": {}
-      }
+      if params["request"]["intent"]["name"] == "AMAZON.YesIntent"
+        return Recipe.build_response("Yes!")
+      elsif params["request"]["intent"]["name"] == "AMAZON.NoIntent"
+        return Recipe.build_response("No.")
+      elsif params["request"]["intent"]["name"] == "IngredientList"
+        return Recipe.build_response("Here are your ingredients.")
+      end
     end
-    return response
   end
 
 
