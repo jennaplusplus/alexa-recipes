11d853744c3969f6dbb15633b2cf5c9cbf9ad8c3
Jenna Nichols <jennaplusplus@gmail.com>
2016-02-24 10:52:04 -0800

    bringing in more methods to request model

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -2,57 +2,41 @@ class Recipe
   include Mongoid::Document
   field :name, type: String
 
-  # def self.router(params)
-  #   if params["request"]["type"] == "LaunchRequest"
-  #     self.launch
-  #   elsif params["request"]["type"] == "IntentRequest"
-  #     self.intents(params)
+  # def self.intents(params)
+  #   if params["request"]["intent"]["name"] == "AMAZON.YesIntent"
+  #     if params["session"]["attributes"]["question"] == "list of ingredients"
+  #       Recipe.ingredient_list
+  #     end
+  #   elsif params["request"]["intent"]["name"] == "AMAZON.NoIntent"
+  #     Recipe.build_response({
+  #       text: "Ok.",
+  #       shouldEndSession: true
+  #     })
+  #   elsif params["request"]["intent"]["name"] == "IngredientList"
+  #     Recipe.ingredient_list
+  #   elsif params["request"]["intent"]["name"] == "IngredientAmount"
+  #     Recipe.ingredient_amount(params)
+  #   elsif params["request"]["intent"]["name"] == "IngredientNeeded"
+  #     Recipe.ingredient_amount(params)
   #   end
   # end
 
-  # def self.launch
+  # def self.ingredient_list
+  #   recipe = Recipe.first
+  #   list = "Here are the ingredients for #{recipe["name"]}. "
+  #   recipe["ingredients"].each do |ingredient|
+  #     if ingredient["unit"].nil?
+  #       list += "#{ingredient["measurement"]} #{ingredient["name"]}, "
+  #     else
+  #       list += "#{ingredient["measurement"]} #{ingredient["unit"]} of #{ingredient["name"]}, "
+  #     end
+  #   end
+  #   list += "."
   #   Recipe.build_response({
-  #     text: "Welcome to Recipes! Would you like a list of ingredients?",
-  #     shouldEndSession: false,
-  #     sessionAttributes: {"question": "list of ingredients"}
+  #     text: list,
+  #     shouldEndSession: true
   #   })
   # end
-
-  def self.intents(params)
-    if params["request"]["intent"]["name"] == "AMAZON.YesIntent"
-      if params["session"]["attributes"]["question"] == "list of ingredients"
-        Recipe.ingredient_list
-      end
-    elsif params["request"]["intent"]["name"] == "AMAZON.NoIntent"
-      Recipe.build_response({
-        text: "Ok.",
-        shouldEndSession: true
-      })
-    elsif params["request"]["intent"]["name"] == "IngredientList"
-      Recipe.ingredient_list
-    elsif params["request"]["intent"]["name"] == "IngredientAmount"
-      Recipe.ingredient_amount(params)
-    elsif params["request"]["intent"]["name"] == "IngredientNeeded"
-      Recipe.ingredient_amount(params)
-    end
-  end
-
-  def self.ingredient_list
-    recipe = Recipe.first
-    list = "Here are the ingredients for #{recipe["name"]}. "
-    recipe["ingredients"].each do |ingredient|
-      if ingredient["unit"].nil?
-        list += "#{ingredient["measurement"]} #{ingredient["name"]}, "
-      else
-        list += "#{ingredient["measurement"]} #{ingredient["unit"]} of #{ingredient["name"]}, "
-      end
-    end
-    list += "."
-    Recipe.build_response({
-      text: list,
-      shouldEndSession: true
-    })
-  end
 
   def self.ingredient_amount(params)
     query = params["request"]["intent"]["slots"]["Ingredient"]["value"]
@@ -78,22 +62,5 @@ class Recipe
       })
     end
   end
-  #
-  # def self.build_response(info_hash)
-  #   response = {
-  #     "version": "0.1.1",
-  #     "response": {
-  #       "outputSpeech": {
-  #         "type": "PlainText",
-  #         "text": info_hash[:text]
-  #       },
-  #       "card": nil,
-  #       "reprompt": nil,
-  #       "shouldEndSession": info_hash[:shouldEndSession]
-  #     },
-  #     "sessionAttributes": info_hash[:sessionAttributes]
-  #   }
-  #   return response
-  # end
 
 end
--- app/models/request.rb
+++ app/models/request.rb
@@ -13,9 +13,9 @@ class Request
 
   def route
     if @type == "LaunchRequest"
-      launch
+      self.launch
     elsif @type == "IntentRequest"
-      intent
+      self.intent
     end
   end
 
@@ -26,5 +26,40 @@ class Request
       sessionAttributes: {"question": "list of ingredients"}
     })
   end
+
+  def intent
+    if @intent == "AMAZON.YesIntent"
+      if @session["attributes"]["question"] == "list of ingredients"
+        self.ingredient_list
+      end
+    elsif @intent == "AMAZON.NoIntent"
+      Response.new({
+        text: "Ok.",
+        shouldEndSession: true
+      })
+    elsif @intent == "IngredientList"
+      Recipe.ingredient_list
+    elsif @intent == "IngredientAmount" || @intent == "IngredientNeeded"
+      Recipe.ingredient_amount(params)
+    end
+  end
+
+  def ingredient_list
+    recipe = Recipe.first
+    list = "Here are the ingredients for #{recipe["name"]}. "
+    recipe["ingredients"].each do |ingredient|
+      if ingredient["unit"].nil?
+        list += "#{ingredient["measurement"]} #{ingredient["name"]}, "
+      else
+        list += "#{ingredient["measurement"]} #{ingredient["unit"]} of #{ingredient["name"]}, "
+      end
+    end
+    list += "."
+    Response.new({
+      text: list,
+      shouldEndSession: true
+    })
+  end
+
 
 end
