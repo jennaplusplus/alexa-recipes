f050b84fffe0e60b88885f9d63791d83b56a0de0
Jenna Nichols <jennaplusplus@gmail.com>
2016-03-07 11:24:45 -0800

    more tests for request class

--- spec/factories.rb
+++ spec/factories.rb
@@ -4,6 +4,10 @@ FactoryGirl.define do
     "email#{n}@example.com"
   end
 
+  sequence :amazon_id do |n|
+    "amazon_#{n}"
+  end
+
   factory :recipe do
     association :user, strategy: :create
     name "Chocolate Chip Cookies"
@@ -13,7 +17,7 @@ FactoryGirl.define do
     notes "this recipe is awesome."
     servings 18
     current_step 2
-    ingredients [{"name"=>"butter", "measurement"=>"1/2", "unit"=>"cup"}, {"name"=>"vanilla extract", "measurement"=>"1/2", "unit"=>"teaspoon"}, {"name"=>"chocolate chips", "measurement"=>"1", "unit"=>"cup"}, {"name"=>"salt", "measurement"=>"1/4", "unit"=>"teaspoon"}, {"name"=>"peanut butter", "measurement"=>"1/2", "unit"=>"cup"}, {"name"=>"egg", "measurement"=>"1"}]
+    ingredients [{"name"=>"butter", "measurement"=>"1/2", "unit"=>"cup"}, {"name"=>"vanilla extract", "measurement"=>"1/2", "unit"=>"teaspoon"}, {"name"=>"chocolate chips", "measurement"=>"1", "unit"=>"cup"}, {"name"=>"salt", "measurement"=>"1/4", "unit"=>"teaspoon"}, {"name"=>"peanut butter", "measurement"=>"1/2", "unit"=>"cup"}, {"name"=>"egg", "measurement"=>"1"}, {"name"=>"chocolate bar", "measurement"=>"1", "unit"=>"cup"}, {"name"=>"chocolate candy", "measurement"=>"1", "unit"=>"cup"}]
     steps ["Preheat oven to 350 degrees F", "In a medium bowl, cream together the butter, white sugar and brown sugar until smooth.", "Stir in the peanut butter, vanilla and egg until well blended.", "Combine the flour, baking soda and salt; stir into the batter just until moistened.", "Mix in the oats and chocolate chips until evenly distributed.", "Drop by tablespoonfuls on to lightly greased cookie sheets.", "Bake for 10 to 12 minutes in the preheated oven, until the edges start to brown.", "Cool on cookie sheets for about 5 minutes before transferring to wire racks to cool completely."]
   end
 
@@ -21,7 +25,7 @@ FactoryGirl.define do
     name "Test Subject"
     email { generate(:email) }
     password "password"
-    amazon_id "amazon_blahhh"
+    amazon_id { generate(:amazon_id) }
   end
 
 end
--- spec/models/request_spec.rb
+++ spec/models/request_spec.rb
@@ -2,74 +2,292 @@ require 'rails_helper'
 
 RSpec.describe Request, type: :model do
 
-  before(:all) do
-    @user = create(:user)
-    @launch_params = {
-      "session": {
-        "sessionId": "SessionId.26f49f30-55a2-4a56-857d-27f7d8782d1e",
-        "application": {
-          "applicationId": "amzn1.echo-sdk-ams.app.34e7d659-d6bc-471e-8ed7-f9fb8cb2f146"
-        },
-        "user": {
-          "userId": "amazon_blahhh"
-        },
-        "new": true
-      },
-      "request": {
-        "type": "LaunchRequest",
-        "requestId": "EdwRequestId.749a4526-728b-45be-bf9b-a7bbcece8a86",
-        "timestamp": "2016-03-07T17:36:51Z"
-      }
-    }.as_json
-    @intent_params = {
-      "session": {
-        "sessionId": "SessionId.125eb41a-bee9-487f-8ced-17119994a951",
-        "application": {
-          "applicationId": "amzn1.echo-sdk-ams.app.34e7d659-d6bc-471e-8ed7-f9fb8cb2f146"
-        },
-        "user": {
-          "userId": "amazon_blahhh"
-        },
-        "new": true
-      },
-      "request": {
-        "type": "IntentRequest",
-        "requestId": "EdwRequestId.3fb29ff6-3f44-417c-854e-9b18cc00577f",
-        "timestamp": "2016-03-07T18:17:24Z",
-        "intent": {
-          "name": "RecipeList",
-          "slots": {}
-        }
-      }
-    }.as_json
-  end
-
-
   describe "#new" do
+    let(:launch_params) do
+      {
+        "session": {
+          "user": {
+            "userId": "new_id"
+          },
+        },
+        "request": {
+          "type": "LaunchRequest",
+        }
+      }.as_json
+    end
+
+    let(:intent_params) do
+      {
+        "session": {
+          "user": {
+            "userId": "new_id"
+          },
+        },
+        "request": {
+          "type": "IntentRequest",
+          "intent": {
+            "name": "RecipeList",
+            "slots": {}
+          }
+        }
+      }.as_json
+    end
     context "launch request" do
       it "creates a request object from params" do
-        expect(Request.new(@launch_params)).to be_an_instance_of Request
+        expect(Request.new(launch_params)).to be_an_instance_of Request
       end
     end
     context "intent request" do
       it "creates a request object from params" do
-        expect(Request.new(@intent_params)).to be_an_instance_of Request
+        expect(Request.new(intent_params)).to be_an_instance_of Request
       end
     end
   end
 
   describe "#route" do
+    let(:user) do
+       create(:user)
+    end
+
+    let(:launch_params) do
+      {
+        "session": {
+          "user": {
+            "userId": user.amazon_id
+          },
+        },
+        "request": {
+          "type": "LaunchRequest",
+        }
+      }.as_json
+    end
+
+    let(:intent_params) do
+      {
+        "session": {
+          "user": {
+            "userId": user.amazon_id
+          },
+        },
+        "request": {
+          "type": "IntentRequest",
+          "intent": {
+            "name": "RecipeList",
+            "slots": {}
+          }
+        }
+      }.as_json
+    end
     context "launch request" do
       it "returns a response" do
-        req = Request.new(@launch_params)
+        req = Request.new(launch_params)
         expect(req.route).to be_an_instance_of Response
       end
     end
     context "intent request" do
       it "returns a response" do
-        req = Request.new(@intent_params)
+        req = Request.new(intent_params)
         expect(req.route).to be_an_instance_of Response
       end
+    end
+  end
+
+  describe "#recipe_list" do
+    let(:user) do
+      create(:user)
+    end
+
+    let(:intent_params) do
+      {
+        "session": {
+          "user": {
+            "userId": user.amazon_id
+          },
+        },
+        "request": {
+          "type": "IntentRequest",
+          "intent": {
+            "name": "RecipeList",
+            "slots": {}
+          }
+        }
+      }.as_json
+    end
+
+    context "the user has no recipes" do
+      it "returns a response" do
+        req = Request.new(intent_params)
+        expect(req.user.recipes.count).to eq 0
+        expect(req.route).to be_an_instance_of Response
+      end
+    end
+    context "the user has one recipe" do
+      it "returns a response" do
+        req = Request.new(intent_params)
+        Recipe.create(user_id: req.user.id)
+        expect(req.user.recipes.count).to eq 1
+        expect(req.recipe_list).to be_an_instance_of Response
+      end
+    end
+    context "the user has multiple recipes" do
+      it "returns a response" do
+        req = Request.new(intent_params)
+        create(:recipe, user_id: req.user.id)
+        create(:recipe, user_id: req.user.id)
+        expect(req.user.recipes.count).to eq 2
+        expect(req.recipe_list).to be_an_instance_of Response
+      end
+    end
+  end
+
+  describe "#get_ingredient" do
+    let(:recipe) do
+      create(:recipe)
+    end
+
+    let(:one_match_params) do
+      {
+        "session": {
+          "user": {
+            "userId": recipe.user.amazon_id
+          },
+        },
+        "request": {
+          "type": "IntentRequest",
+          "intent": {
+            "name": "GetIngredient",
+            "slots": {
+              "Ingredient": {
+                "name": "Ingredient",
+                "value": "salt"
+              }
+            }
+          }
+        }
+      }.as_json
+    end
+
+    let(:two_match_params) do
+      {
+        "session": {
+          "user": {
+            "userId": recipe.user.amazon_id
+          },
+        },
+        "request": {
+          "type": "IntentRequest",
+          "intent": {
+            "name": "GetIngredient",
+            "slots": {
+              "Ingredient": {
+                "name": "Ingredient",
+                "value": "butter"
+              }
+            }
+          }
+        }
+      }.as_json
+    end
+
+    let(:three_match_params) do
+      {
+        "session": {
+          "user": {
+            "userId": recipe.user.amazon_id
+          },
+        },
+        "request": {
+          "type": "IntentRequest",
+          "intent": {
+            "name": "GetIngredient",
+            "slots": {
+              "Ingredient": {
+                "name": "Ingredient",
+                "value": "chocolate"
+              }
+            }
+          }
+        }
+      }.as_json
+    end
+
+    let(:bad_intent_params) do
+      {
+        "session": {
+          "user": {
+            "userId": recipe.user.amazon_id
+          },
+        },
+        "request": {
+          "type": "IntentRequest",
+          "intent": {
+            "name": "GetIngredient",
+            "slots": {
+              "Ingredient": {
+                "name": "Ingredient"
+              }
+            }
+          }
+        }
+      }.as_json
+    end
+
+    context "one matched ingredient" do
+      it "returns a response" do
+        req = Request.new(one_match_params)
+        req.user["active_recipe_id"] = recipe.id
+        expect(req.get_ingredient).to be_an_instance_of Response
+      end
+    end
+    context "two matched ingredients" do
+      it "returns a response" do
+        req = Request.new(two_match_params)
+        req.user["active_recipe_id"] = recipe.id
+        expect(req.get_ingredient).to be_an_instance_of Response
+      end
+    end
+    context "more than two matched ingredients" do
+      it "returns a response" do
+        req = Request.new(three_match_params)
+        req.user["active_recipe_id"] = recipe.id
+        expect(req.get_ingredient).to be_an_instance_of Response
+      end
+    end
+    context "the ingredient was not understood" do
+      it "returns a response" do
+        req = Request.new(bad_intent_params)
+        req.user["active_recipe_id"] = recipe.id
+        expect(req.get_ingredient).to be_an_instance_of Response
+      end
+    end
+  end
+
+  describe "#ingredient_list" do
+    let(:recipe) do
+      create(:recipe)
+    end
+
+    let(:intent_params) do
+      {
+        "session": {
+          "user": {
+            "userId": recipe.user.amazon_id
+          },
+        },
+        "request": {
+          "type": "IntentRequest",
+          "intent": {
+            "name": "IngredientList",
+            "slots": {}
+          }
+        }
+      }.as_json
+    end
+
+    it "returns a response" do
+      req = Request.new(intent_params)
+      req.user["active_recipe_id"] = recipe.id
+      expect(req.ingredient_list).to be_an_instance_of Response
     end
   end
 
