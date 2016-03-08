c04dbcc60e1e883d3537ccd1f6c19d37657c13fc
Jenna Nichols <jennaplusplus@gmail.com>
2016-03-08 11:18:21 -0800

    new recipe associated to current_user

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,18 +1,18 @@
 class RecipesController < ApplicationController
 
   def index
+    @recipes = current_user.recipes
   end
 
   def new
-    @user = User.find(params[:user_id])
     @recipe = Recipe.new
   end
 
   def create
-    @recipe = Recipe.new(recipe_params[:recipe])
+    @recipe = current_user.recipes.new(recipe_params[:recipe])
 
     if @recipe.save
-      redirect_to user_recipe_path(user_id: params[:user_id], id: @recipe.id)
+      redirect_to user_recipe_path(user_id: current_user, id: @recipe.id)
     else
       render :new
     end
