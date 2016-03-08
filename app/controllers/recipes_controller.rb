c3f7fa75f2ffb0a6691775186af91fb5a11be830
Jenna Nichols <jennaplusplus@gmail.com>
2016-03-08 10:30:18 -0800

    SUPER basic form submission

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -9,10 +9,24 @@ class RecipesController < ApplicationController
   end
 
   def create
+    @recipe = Recipe.new(recipe_params[:recipe])
 
+    if @recipe.save
+      redirect_to user_recipe_path(user_id: params[:user_id], id: @recipe.id)
+    else
+      render :new
+    end
   end
 
   def show
+    @user = User.find(params[:user_id])
+    @recipe = Recipe.find(params[:id])
+  end
+
+  private
+
+  def recipe_params
+    params.permit(recipe:[:name])
   end
 
 
