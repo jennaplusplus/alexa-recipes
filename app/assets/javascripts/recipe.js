43c629ce92aa80ced89ff6d721a826ed55d55db7
Jenna Nichols <jennaplusplus@gmail.com>
2016-03-09 14:30:24 -0800

    javascript to add more ingredients slots

--- app/assets/javascripts/recipe.js
+++ app/assets/javascripts/recipe.js
@@ -1,3 +1,13 @@
 $( document ).ready(function() {
-    console.log( "ready!" );
+  console.log( "ready!" );
+
+  $("#more-ingredients").click(function () {
+    var $lastRow = $("#ingredients-table tr:last");
+    for (var i = 0; i < 3; i++) {
+      var $newRow = $lastRow.clone();
+      $("#ingredients-table tbody").append($newRow);
+    }
+  });
+
+
 });
