66e34f14780c244e9e65d9775706559b1a456ea5
Jenna Nichols <jennaplusplus@gmail.com>
2016-03-10 15:54:22 -0800

    can delete recipe from show view

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -38,6 +38,12 @@ class RecipesController < ApplicationController
     @recipe = Recipe.find(params[:id])
   end
 
+  def destroy
+    recipe = Recipe.find(params[:id])
+    recipe.destroy
+    redirect_to recipes_path
+  end
+
   private
 
   def recipe_params
