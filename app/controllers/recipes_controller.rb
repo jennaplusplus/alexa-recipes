c30c4cbd09687921db235cca67d979898902c390
Jenna Nichols <jennaplusplus@gmail.com>
2016-03-14 12:35:04 -0700

    order recipes alpha in index view

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -2,7 +2,7 @@ class RecipesController < ApplicationController
   before_action :authenticate_user!
 
   def index
-    @recipes = current_user.recipes
+    @recipes = current_user.recipes.order_by(name: :asc)
   end
 
   def new
