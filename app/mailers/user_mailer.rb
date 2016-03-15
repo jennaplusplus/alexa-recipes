c4090340a197ef5030ad5bd8339a3bb036e00fa9
Jenna Nichols <jennaplusplus@gmail.com>
2016-03-15 13:11:10 -0700

    update mail content again

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -2,6 +2,7 @@ class UserMailer < ApplicationMailer
 
   def welcome(user_id)
     user = User.find(user_id)
+    @url = "https://www.alexarecipes.com/recipes"
     mail to: user.email, subject: "Welcome!"
   end
 end
