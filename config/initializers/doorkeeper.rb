7e1102dbaaea95a9dac67c6d764661216492a45d
Jenna Nichols <jennaplusplus@gmail.com>
2016-04-13 09:08:40 -0700

    trying different doorkeeper config

--- config/initializers/doorkeeper.rb
+++ config/initializers/doorkeeper.rb
@@ -7,7 +7,12 @@ Doorkeeper.configure do
     # fail "Please configure doorkeeper resource_owner_authenticator block located in #{__FILE__}"
     # Put your resource owner authentication logic here.
     # Example implementation:
-      User.find(session[:user_id]) || redirect_to(new_user_session_path)
+      # User.find(session[:user_id]) || redirect_to(new_user_session_path)
+  end
+
+  resource_owner_from_credentials do |_routes|
+    user = User.find_for_database_authentication(email: params[:username])
+    user if user && user.valid_password?(params[:password])
   end
 
   # If you want to restrict access to the web interface for adding oauth authorized applications, you need to declare the block below.
@@ -35,10 +40,10 @@ Doorkeeper.configure do
 
   # Reuse access token for the same resource owner within an application (disabled by default)
   # Rationale: https://github.com/doorkeeper-gem/doorkeeper/issues/383
-  # reuse_access_token
+  reuse_access_token
 
   # Issue access tokens with refresh token (disabled by default)
-  # use_refresh_token
+  use_refresh_token
 
   # Provide support for an owner to be assigned to each registered application (disabled by default)
   # Optional parameter :confirmation => true (default false) if you want to enforce ownership of
@@ -94,6 +99,8 @@ Doorkeeper.configure do
   #   http://tools.ietf.org/html/rfc6819#section-4.4.3
   #
   # grant_flows %w(authorization_code client_credentials)
+  grant_flows %w[password client_credentials]
+
 
   # Under some circumstances you might want to have applications auto-approved,
   # so that the user skips the authorization step.
